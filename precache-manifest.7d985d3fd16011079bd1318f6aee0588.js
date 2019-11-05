e15cf07b8dd462a3efc17110c6b837bf8a36615a
LMBoyle <boylelukem@gmail.com>
2019-11-05 14:11:54 -0500

    Updates

---
+++ precache-manifest.7d985d3fd16011079bd1318f6aee0588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31cef30aad5b42ce21bebd896aa9c244",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "6c9220d6d9559b335ddf",
+    "url": "/clickyGame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6effbca24685ced80aa",
+    "url": "/clickyGame/static/js/2.61f2fa50.chunk.js"
+  },
+  {
+    "revision": "6c9220d6d9559b335ddf",
+    "url": "/clickyGame/static/js/main.822b81e9.chunk.js"
+  },
+  {
+    "revision": "37ddb85b923bdd0f12d0",
+    "url": "/clickyGame/static/js/runtime-main.49a6f09a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clickyGame/static/media/logo.25bf045c.svg"
+  }
+]);
