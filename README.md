0662011b02be60301ebb8c8d5e8ad792d25121f3
LMBoyle <boylelukem@gmail.com>
2019-11-09 11:12:39 -0500

    updated readme and added alert if your score reaches 12

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # clickyGame
+Test your memory with this fun, Rick and Morty themed game. 
+
+## Instructions
+Click on each character, but only click each one once. Click on a character more than once, it's game over!
+
+## Languages/Technologies 
+* React
+* HTML
+* Bootstrap
+* JavaScript
+
+## What It Teaches
+How to use react to build a website.
+
+## Demo
+![Demo Gif]()
+
+## Future Development/Improvements
+- [ ] Mobile Responsive
+- [ ] Add other themes
+
+## Links
+[Deployed Link](https://lmboyle.github.io/clickyGame/)
+
+[Portfolio](https://lmboyle.github.io/)
--- src/App.js
+++ src/App.js
@@ -9,73 +9,78 @@ import Title from "./components/Title/Title";
 import Score from "./components/Score/Score";
 import characters from "./characters.json"
 
-// Export ====================================================================
-
-export default
 // Functions =================================================================
-  class App extends Component {
-    state = {
-      characters,
-      score: 0,
-    };
 
-    // Shuffle the cards
-    shuffle = () => {
-      this.setState({
-        characters: this.state.characters.sort(function(a,b){
-          return 0.5 - Math.random()
-        })
+class App extends Component {
+  state = {
+    characters,
+    score: 0,
+  };
+
+  // Shuffle the cards
+  shuffle = () => {
+    this.setState({
+      characters: this.state.characters.sort(function(a,b){
+        return 0.5 - Math.random()
       })
+    })
+  }
+
+  // When clicked, set the character to clicked
+  setClicked = id => {
+    console.log("you clicked on the " + id + " character");
+    // Find the character with the id of the clicked card
+    let clickedChar = this.state.characters.find(char => char.id === id);
+
+    // If is clicked is false
+    if (!clickedChar.isClicked) {
+      clickedChar.isClicked = true
+    
+      this.setState({
+        characters: this.state.characters,
+        score: this.state.score + 1
+      })
+      // Shuffle
+      this.shuffle()
+      console.log("Updated\n") 
+      this.state.characters.map(character => console.log(character.name + " " + character.isClicked))
+      console.log("================================================================")
     }
-
-    // When clicked, set the character to clicked
-    setClicked = id => {
-      console.log("you clicked on the " + id + " character");
-      // Find the character with the id of the clicked card
-      let clickedChar = this.state.characters.find(char => char.id === id);
-
-      // If is clicked is false
-      if (!clickedChar.isClicked) {
-        clickedChar.isClicked = true
-      
-        this.setState({
-          characters: this.state.characters,
-          score: this.state.score + 1
-        })
-        // Shuffle
-        this.shuffle()
-        console.log("Updated\n") 
-        this.state.characters.map(character => console.log(character.name + " " + character.isClicked))
-        console.log("================================================================")
+    // If is clicked is true
+    else {
+      alert("Game Over!")
+      this.setState({ 
+        characters: this.state.characters.map(character => { return { ...character, clicked : false } }),
+        score: 0
+      })
+      if (this.state.score === 12) {
+        alert("You win!")
       }
-      // If is clicked is true
-      else {
-        alert("Game Over!")
-        this.setState({ 
-          characters: this.state.characters.map(character => { return { ...character, clicked : false } }),
-          score: 0
-        })
-        // Shuffle
-        this.shuffle()
-      }
-    }
-
-    render() {
-      return (
-        <Wrapper>
-          <HeadDiv>
-            <Title> Rick and Morty Clicky Game </Title>
-            <Score> Score: {this.state.score} </Score>
-          </HeadDiv>
-
-          {this.state.characters.map(character => (
-            <CharCard
-              key={character.id}
-              characters={character}
-              onClick={() => this.setClicked(character.id)}
-            />
-          ))}
-        </Wrapper>
-      );
+      // Shuffle
+      this.shuffle()
     }
   }
+
+  render() {
+    return (
+      <Wrapper>
+        <HeadDiv>
+          <Title> Rick and Morty Clicky Game </Title>
+          <Score> Score: {this.state.score} </Score>
+        </HeadDiv>
+
+        {this.state.characters.map(character => (
+          <CharCard
+            key={character.id}
+            characters={character}
+            onClick={() => this.setClicked(character.id)}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+// Export ====================================================================
+
+export default App
